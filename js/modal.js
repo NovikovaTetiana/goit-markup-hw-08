1f6030c71709bb566e9d18d5dfd99f567b41c3f4
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2022-12-16 21:13:06 +0200

    modal js

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
         openModalBtn: document.querySelector("[data-modal-open]"),
         closeModalBtn: document.querySelector("[data-modal-close]"),
         modal: document.querySelector("[data-modal]"),
+        body: document.querySelector('body'),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
@@ -11,10 +12,12 @@
 
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
+        refs.body.classList.toggle('no-scroll');
     }
 
     function removeModal() {
         refs.modal.classList.add('is-hidden');
+        refs.body.classList.remove('no-scroll');
       }
 
 })();
